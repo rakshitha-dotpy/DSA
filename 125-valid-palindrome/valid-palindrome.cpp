3de92a65bcee2c691d2ef27b5f1add112f06c65f
Rakshitha <gvrakshithaofficial@gmail.com>
2026-07-29 22:30:11 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (24.28%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string temp = "";
+
+        for (char c : s) {
+            if (isalnum(c)) {
+                temp += tolower(c);
+            }
+        }
+
+        int left = 0;
+        int right = temp.length() - 1;
+
+        while (left < right) {
+            if (temp[left] != temp[right]) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+};
